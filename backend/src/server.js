7e6493eadda0714f230af09fe96d2480b4654324
Sumit00s <sonawanesumit245@gmail.com>
2025-08-28 12:38:44 +0530

    Comment Routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/",(req,res)=>{
 
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/comments",commentRoutes);
 
 // Error handling middleware
 app.use((err,req,res)=>{
